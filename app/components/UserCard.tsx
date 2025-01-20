03abffa8f7072f42320572850878cbccc39f15cf
imsherr <imsherrill@gmail.com>
2025-01-20 07:32:03 -0800

    small

--- app/components/UserCard.tsx
+++ app/components/UserCard.tsx
@@ -7,7 +7,10 @@ type UserCardProps = {
 export function UserCard({ user }: UserCardProps) {
   return (
     <div className="bg-white p-4 rounded-lg shadow hover:shadow-md transition-shadow">
-      <p className="text-gray-600">{user.id}</p>
+      <p className="font-small text-gray-900">
+        <span className="text-gray-400">id:</span> {user.id}
+      </p>
+
       <p className="font-semibold text-lg text-gray-900">{user.name}</p>
     </div>
   );
